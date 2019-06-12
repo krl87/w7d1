f5b596c9a4390e72da820b85143b3844cc88d9ce
Kayley <kayley.luftig@gmail.com>
2019-06-12 16:04:55 +0000

    add question functionality to initialize, generate random number, ask addition question, and check answer

--- math-game/question.rb
+++ math-game/question.rb
@@ -1,5 +1,27 @@
 require('./player')
 
 class Question
+  attr_reader :number1, :number2, :string, :sum
 
+  def initialize
+    @number1 = 0
+    @number2 = 0
+    @sum = number1 + number2
+  end
+
+  def generate_number
+    @number1 = rand(1..20)
+    @number2 = rand(1..20)
+    @sum = @number1 + @number2
+    @string = "What does #{number1} plus #{number2} equal?"
+  end
+
+  def ask_question
+    answer = gets.chomp.to_i
+    if answer == @sum
+      return true
+    else
+      return false
+    end
+  end
 end
